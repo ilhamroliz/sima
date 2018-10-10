231bcce44890a59fc9bf512ec46f9d816599f13e
ilhamroliz <ilham.rolis@gmail.com>
2018-10-10 14:07:20 +0700

    roliz

--- alamraya/resources/views/manajemen-project/project-progress/project-progress.blade.php
+++ alamraya/resources/views/manajemen-project/project-progress/project-progress.blade.php
@@ -680,7 +680,8 @@
                 success: function (response) {
                     $('#modal-progress').modal('show');
                     var data = response.data[0];
-                    if ((posisi == 'PRJPRG' || posisi == 'PRJLED') && (data.pp_state == 'ENTRY' || data.pp_state == 'REVISION')) {
+                    if ((data.pp_state == 'ENTRY' || data.pp_state == 'REVISION')) {
+                    /*if ((posisi == 'PRJPRG' || posisi == 'PRJLED') && (data.pp_state == 'ENTRY' || data.pp_state == 'REVISION')) {*/
                         $('#edit-target').prop('readonly', true);
                         $('#edit-execution').prop('readonly', false);
                     }
